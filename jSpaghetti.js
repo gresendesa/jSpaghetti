0bfe2e2c79f8e9ba8a41f479d7429f9dc341f4a7
Raiane Resende <Raiane Resende>
2016-05-03 00:12:27 -0300

    -Adding an alert to notificate if instructions array are empty

--- jSpaghetti.js
+++ jSpaghetti.js
@@ -71,37 +71,41 @@
 
 	/*It checks if there is an error on instruction commands*/
 	function checkInstructionsSyntax(instructions, procedures){
-		for (var i = 0; i < instructions.length; i++) {
-			var instructionLabel = getFirstAttribName(instructions[i])
-			if (typeof instructions[i][instructionLabel] == 'string'){ //It turns string intruction content into a array
-				instructions[i][instructionLabel] = [instructions[i][instructionLabel]]
-			}
-			for (var z = 0; z < instructions[i][instructionLabel].length; z++) {
-				switch(typeof(instructions[i][instructionLabel][z])){
-					case 'object':
-						var internalComName = getFirstAttribName(instructions[i][instructionLabel][z])
-						if (INTERNAL_OBJECT_COMMANDS_LIST.indexOf(internalComName) == -1) return "Internal command \"" + internalComName + "\" is undefined"
-						var comContent = instructions[i][instructionLabel][z][internalComName]
-						if (internalComName == GOTOIF_COMMAND){
-							if (Object.prototype.toString.call(comContent) == '[object Array]'){
-								if (comContent.length < 2) return "Internal command \"" + internalComName + "\" must receive an array that must have at least 2 positions"
-								for (var y = 1; y < comContent.length; y++) {
-									var intructionLabel = comContent[y]
-									if ((getInstructionPosByLabel(intructionLabel, instructions) === false) && (intructionLabel != EXIT_COMMAND)) return "Instruction label \"" + intructionLabel + "\" is undefined"
+		if (instructions.length > 0){
+			for (var i = 0; i < instructions.length; i++) {
+				var instructionLabel = getFirstAttribName(instructions[i])
+				if (typeof instructions[i][instructionLabel] == 'string'){ //It turns string intruction content into a array
+					instructions[i][instructionLabel] = [instructions[i][instructionLabel]]
+				}
+				for (var z = 0; z < instructions[i][instructionLabel].length; z++) {
+					switch(typeof(instructions[i][instructionLabel][z])){
+						case 'object':
+							var internalComName = getFirstAttribName(instructions[i][instructionLabel][z])
+							if (INTERNAL_OBJECT_COMMANDS_LIST.indexOf(internalComName) == -1) return "Internal command \"" + internalComName + "\" is undefined"
+							var comContent = instructions[i][instructionLabel][z][internalComName]
+							if (internalComName == GOTOIF_COMMAND){
+								if (Object.prototype.toString.call(comContent) == '[object Array]'){
+									if (comContent.length < 2) return "Internal command \"" + internalComName + "\" must receive an array that must have at least 2 positions"
+									for (var y = 1; y < comContent.length; y++) {
+										var intructionLabel = comContent[y]
+										if ((getInstructionPosByLabel(intructionLabel, instructions) === false) && (intructionLabel != EXIT_COMMAND)) return "Instruction label \"" + intructionLabel + "\" is undefined"
+									}
+								} else {
+									return "Internal command \"" + internalComName + "\" must receive an array"
 								}
-							} else {
-								return "Internal command \"" + internalComName + "\" must receive an array"
 							}
-						}
-						break
-					default:
-						var procedureName = instructions[i][instructionLabel][z]
-						if (!procedures.hasOwnProperty(procedureName) && (procedureName != EXIT_COMMAND)) return "Procedure \"" + procedureName + "\" is undefined"
-						break
+							break
+						default:
+							var procedureName = instructions[i][instructionLabel][z]
+							if (!procedures.hasOwnProperty(procedureName) && (procedureName != EXIT_COMMAND)) return "Procedure \"" + procedureName + "\" is undefined"
+							break
+					}
 				}
 			}
+			return true
+		} else {
+			return "There are no instructions to be executed"
 		}
-		return true
 	}
 
 	/*This function shows a custom message error on browser console*/
